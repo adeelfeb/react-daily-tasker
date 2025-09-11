c0148efa47cd04ee385f1c17e2922e7993019624
adeelfeb <at4563323@gmail.com>
2025-09-11 17:04:51 +0500

    Deduplicate UK cities in constants to streamline the list and improve maintainability. Updated comments for clarity.

--- event_calendar/src/constants/index.js
+++ event_calendar/src/constants/index.js
@@ -23,18 +23,15 @@ export const CALENDAR_VIEWS = {
   AGENDA: 'agenda'
 };
 
-// UK Cities
+// UK Cities (deduplicated)
 export const UK_CITIES = [
   // England - Major Cities
   "London", "Birmingham", "Manchester", "Leeds", "Sheffield", "Bradford", "Liverpool", "Bristol", "Coventry", "Leicester",
   "Nottingham", "Newcastle upon Tyne", "Stoke-on-Trent", "Southampton", "Derby", "Portsmouth", "Brighton and Hove", "Plymouth", "Northampton", "Reading",
   "Luton", "Wolverhampton", "Bolton", "Bournemouth", "Norwich", "Swindon", "Huddersfield", "York", "Ipswich", "Blackpool",
-  "Middlesbrough", "Hull", "West Bromwich", "Peterborough", "Stockport", "Stoke-on-Trent", "Telford", "Exeter", "Southend-on-Sea", "Walsall",
+  "Middlesbrough", "Hull", "West Bromwich", "Peterborough", "Stockport", "Telford", "Exeter", "Southend-on-Sea", "Walsall",
   "Maidstone", "Blackburn", "Oldham", "Woking", "Cheltenham", "Chelmsford", "Colchester", "Crawley", "Gillingham", "Basingstoke",
-  "Worcester", "Maidstone", "Rotherham", "Stockton-on-Tees", "Worthing", "Hastings", "Mansfield", "Oxford", "Ipswich", "Slough",
-  "Eastbourne", "Blackpool", "Gloucester", "Telford", "Southend-on-Sea", "Walsall", "Maidstone", "Blackburn", "Oldham", "Woking",
-  "Cheltenham", "Chelmsford", "Colchester", "Crawley", "Gillingham", "Basingstoke", "Worcester", "Maidstone", "Rotherham", "Stockton-on-Tees",
-  "Worthing", "Hastings", "Mansfield", "Oxford", "Ipswich", "Slough", "Eastbourne", "Blackpool", "Gloucester", "Telford",
+  "Worcester", "Rotherham", "Stockton-on-Tees", "Worthing", "Hastings", "Mansfield", "Oxford", "Slough", "Eastbourne", "Gloucester",
   
   // Scotland - Major Cities
   "Edinburgh", "Glasgow", "Aberdeen", "Dundee", "Stirling", "Perth", "Inverness", "Dumfries", "Ayr", "Kilmarnock",
